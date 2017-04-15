a9bfb330fc58b15393b78cf33723e1be131c7db5
HarryStevens <harryjosephstevens@gmail.com>
2017-04-15 16:41:32 +0530

    Move from archive API to articles API for significant speed improvement

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-// // dependencies
+// dependencies
 var request = require("request"), 
 	fs = require("fs"),
-	Twit = require("twit");
+  _ = require("underscore"),
+	Twit = require("twit"),
+  moment = require('moment');
 
 // a new Twit instance
 var T = new Twit({
@@ -11,83 +13,122 @@ var T = new Twit({
   access_token_secret: "pvSWeBsVfuwT6CjXAlp42gLRLAoYW1bydg3bWOQSsbYeM"
 });
 
+// underscore rateLimit function
+_.rateLimit = function(func, rate, async) {
+  var queue = [];
+  var timeOutRef = false;
+  var currentlyEmptyingQueue = false;
+  
+  var emptyQueue = function() {
+    if (queue.length) {
+      currentlyEmptyingQueue = true;
+      _.delay(function() {
+        if (async) {
+          _.defer(function() { queue.shift().call(); });
+        } else {
+          queue.shift().call();
+        }
+        emptyQueue();
+      }, rate);
+    } else {
+      currentlyEmptyingQueue = false;
+    }
+  };
+  
+  return function() {
+    var args = _.map(arguments, function(e) { return e; }); // get arguments into an array
+    queue.push( _.bind.apply(this, [func, this].concat(args)) ); // call apply so that we can pass in arguments as parameters as opposed to an array
+    if (!currentlyEmptyingQueue) { emptyQueue(); }
+  };
+};
+
 // an empty array
 var tweets = [];
 
-// date parser
-var date;
-new Date().toISOString().split("T")[0].split("-").forEach((d, i) => {
-  date = i == 0 ? d - 100 + "-" : i == 1 ? date + d + "-" : date + d;
-});
+// use moment for date parser
+var date = moment().subtract(100, "years"); // "today", but actually 100 years ago
 
 // log to get started...
-console.log("Getting tweets from " + date);
+console.log("Getting articles from " + date.format("MMMM Do, YYYY") + ".");
+console.log(" ")
 
-// request articles from nytimes
-request.get(
-  {
-    url: "https://api.nytimes.com/svc/archive/v1/1917/4.json",
-    qs: {
-      "api-key": "6ec2da195c80457a827aa558bbb82f95"
-    }
+
+// each page only returns 10 requests, so we'll find out how many there are first
+request.get({
+  url: "https://api.nytimes.com/svc/search/v2/articlesearch.json",
+  qs: {
+    'api-key': "6ec2da195c80457a827aa558bbb82f95",
+    'q': "russia",
+    'begin_date': date.format("YYYYMMDD"),
+    'end_date': date.format("YYYYMMDD"),
   },
-  (err, response, body) => {
-    // this is the response body
-    body = JSON.parse(body);
-    var docs = body.response.docs;
+}, function(err, response, body) {
+  var hits = JSON.parse(body).response.meta.hits;
+  var pages = Math.ceil(hits / 10);
+  console.log("Found " + hits + " articles on " + pages + " pages of results.");
+  console.log(" ");
 
-    // write filtered tweets to json
-    docs.forEach((d, i) => {
-    	
-      // an empty object to store data
-      var obj = {};
+  // a rate-limited version of the request
+  var makeRequest_limited = _.rateLimit(makeRequest, 10000);
+  for (var i = 0; i < pages; i++){
+    makeRequest_limited(i);
+  }
 
-      obj.headline = d.headline.main;
-      obj.url = "http://query.nytimes.com/mem/archive-free/pdf?res=" + d.web_url.split("res=")[1];
-      obj.date = d.pub_date.split("T")[0];
+  // so now we'll actually get the articles based on the number of pages
+  function makeRequest(page){
+    request.get({
+      url: "https://api.nytimes.com/svc/search/v2/articlesearch.json",
+      qs: {
+        'api-key': "6ec2da195c80457a827aa558bbb82f95",
+        'q': "russia",
+        'begin_date': date.format("YYYYMMDD"),
+        'end_date': date.format("YYYYMMDD"),
+        'page': page
+      },
+    }, function(err, response, body) {
+      
+      body = JSON.parse(body);
+      var docs = body.response.docs;
 
-      var tweet_end = obj.url + " #1917LIVE";
+      //write filtered tweets to json
+      docs.forEach((d, i) => {
+        
+        // an empty object to store data
+        var obj = {};
 
-      // the tweet content is dependent upon the length of the headline
-      // if the headline is too long, we'll cut it off and add three dots
-      if (obj.headline.length > 103) {
-        obj.tweet = obj.headline.substr(0, 103) + "... " + tweet_end;
-      } else {
-        obj.tweet = obj.headline + " " + tweet_end;
-      }
+        obj.day_index = i;
+        obj.headline = d.headline.main;
+        obj.url = "http://query.nytimes.com/mem/archive-free/pdf?res=" + d.web_url.split("res=")[1];
+        obj.date = d.pub_date.split("T")[0];
 
-      // some dependencies to filter out only tweets we want
-      // they must be from the correct date and they must be about either russia or germany
-      if (
-        obj.date == date &&
-        (
-        	obj.headline.toLowerCase().includes("russia") ||
-          (d.lead_paragraph && d.lead_paragraph.toLowerCase().includes("russia")) ||
-          (d.snippet && d.snippet.toLowerCase().includes("russia")) ||
-          obj.headline.toLowerCase().includes("lenin") ||
-          (d.lead_paragraph && d.lead_paragraph.toLowerCase().includes("lenin")) ||
-          (d.snippet && d.snippet.toLowerCase().includes("lenin")) ||
-          obj.headline.toLowerCase().includes("petrograd") ||
-          (d.lead_paragraph && d.lead_paragraph.toLowerCase().includes("petrograd")) ||
-          (d.snippet && d.snippet.toLowerCase().includes("petrograd"))
-         )
-      ) {
+        var tweet_end = obj.url + " #1917LIVE";
+
+        // the tweet content is dependent upon the length of the headline
+        // if the headline is too long, we'll cut it off and add three dots
+        if (obj.headline.length > 103) {
+          obj.tweet = obj.headline.substr(0, 103) + "... " + tweet_end;
+        } else {
+          obj.tweet = obj.headline + " " + tweet_end;
+        }
 
         // post to twitter
         T.post("statuses/update", { status: obj.tweet }, (err, data, response) => {
-        	if (!err){
+          if (!err){
             console.log(data.text);
             console.log(" ");
-        	} else {
-        		console.log(err.message);
-        	}
+          } else {
+            console.log(err.message);
+          }
         });
 
         // and we'll save the tweets for fun
         tweets.push(obj)
-        fs.writeFileSync("tweets/tweets_" + date + ".json", JSON.stringify(tweets));
+        fs.writeFileSync("tweets/tweets_" + date.format("YYYY-MM-DD") + ".json", JSON.stringify(tweets));
 
-      } // end if filter
-    }); // end forEach()
+      });
+
+    });
+
   }
-); // end request
+
+});
