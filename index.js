5d7660b202bef2b03e7749395b9f70fe59d0d1fd
HarryStevens <harryjosephstevens@gmail.com>
2017-04-15 18:11:06 +0530

    Better tweet parser

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ request.get({
   console.log(" ");
 
   // a rate-limited version of the request
-  var makeRequest_limited = _.rateLimit(makeRequest, 1000);
+  var makeRequest_limited = _.rateLimit(makeRequest, 5000);
   for (var i = 0; i < pages; i++){
     makeRequest_limited(i);
   }
@@ -79,7 +79,7 @@ request.get({
       url: "https://api.nytimes.com/svc/search/v2/articlesearch.json",
       qs: {
         'api-key': "6ec2da195c80457a827aa558bbb82f95",
-        'q': "russia",
+        "fq": "russia OR lenin OR trotsky OR germany",
         'begin_date': date.format("YYYYMMDD"),
         'end_date': date.format("YYYYMMDD"),
         'page': page
@@ -95,33 +95,42 @@ request.get({
         // an empty object to store data
         var obj = {};
 
-        obj.day_index = i;
-        obj.headline = d.headline.main;
+        obj.page = page + 1;
+        obj.page_index = i + 1;
         obj.url = "http://query.nytimes.com/mem/archive-free/pdf?res=" + d.web_url.split("res=")[1];
         obj.date = d.pub_date.split("T")[0];
 
+        // create the tweet, including cleaning headline to make more readable
+        var tweet_start = d.headline.main;
         var tweet_end = obj.url + " #1917LIVE";
 
         // the tweet content is dependent upon the length of the headline
-        // if the headline is too long, we'll cut it off and add three dots
-        if (obj.headline.length > 103) {
-          obj.tweet = obj.headline.substr(0, 103) + "... " + tweet_end;
-        } else {
-          obj.tweet = obj.headline + " " + tweet_end;
-        }
+        // if the headline is too long, we'll cut it off, if necessary adding 3 dots
+        if (tweet_start.length > 106) {
+
+          // first we'll split on the semicolon and check again
+          tweet_start = tweet_start.split(";")[0];
+
+          // if it's still longer, we'll do some elipses
+          if (tweet_start.length > 103){
+            tweet_start = tweet_start.substr(0, 103);
+            // put the elipses after the space
+            var li = tweet_start.lastIndexOf(" ");
+            tweet_start = tweet_start.substr(0, li) + "...";
+          }
+        } 
+
+        obj.tweet = tweet_start.toTitleCase() + " " + tweet_end;
 
         // post to twitter
-        // T.post("statuses/update", { status: obj.tweet }, (err, data, response) => {
-        //   if (!err){
-        //     console.log(data.text);
-        //     console.log(" ");
-        //   } else {
-        //     console.log(err.message);
-        //   }
-        // });
-
-        console.log(obj.url);
-        console.log(" ");
+        T.post("statuses/update", { status: obj.tweet }, (err, data, response) => {
+          if (!err){
+            console.log(data.text);
+            console.log(" ");
+          } else {
+            console.log(err.message);
+          }
+        });
 
         // and we'll save the tweets for fun
         tweets.push(obj)
@@ -134,3 +143,40 @@ request.get({
   }
 
 });
+
+String.prototype.toTitleCase = function() {
+  var i, j, str, lowers, uppers;
+  str = this.replace(/([^\W_]+[^\s-]*) */g, function(txt) {
+    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+  });
+
+  // Certain minor words should be left lowercase unless 
+  // they are the first or last words in the string
+  lowers = ['A', 'An', 'The', 'And', 'But', 'Or', 'For', 'Nor', 'As', 'At', 
+  'By', 'For', 'From', 'In', 'Into', 'Near', 'Of', 'On', 'Onto', 'To', 'With'];
+  for (i = 0, j = lowers.length; i < j; i++)
+    str = str.replace(new RegExp('\\s' + lowers[i] + '\\s', 'g'), 
+      function(txt) {
+        return txt.toLowerCase();
+      });
+
+  // Certain words such as initialisms or acronyms should be left uppercase
+  uppers = ['Id', 'Tv'];
+  for (i = 0, j = uppers.length; i < j; i++)
+    str = str.replace(new RegExp('\\b' + uppers[i] + '\\b', 'g'), 
+      uppers[i].toUpperCase());
+
+  
+  // others
+  [{a:"U.s.",b:"U.S."}]
+    .forEach(function(d,i){
+      str = replaceAll(str, d.a, d.b);    
+    });
+  
+  function replaceAll(string, search, replacement) {
+    var target = string;
+    return target.replace(new RegExp(search, 'g'), replacement);
+  };
+
+  return str;
+}
