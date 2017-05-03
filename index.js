aaf0922ca75946b2ebef2cbd2bc465a5411bdd7b
HarryStevens <harryjosephstevens@gmail.com>
2017-05-03 14:43:48 +0530

    Update replacements

--- index.js
+++ index.js
@@ -339,7 +339,7 @@ String.prototype.toTitleCase = function() {
   str = this.replace(/([^\W_]+[^\s-]*) */g, function(txt) {
     return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
   });
-
+  
   // Certain minor words should be left lowercase unless 
   // they are the first or last words in the string
   lowers = ['A', 'An', 'The', 'And', 'But', 'Or', 'For', 'Nor', 'As', 'At', 
@@ -366,7 +366,12 @@ String.prototype.toTitleCase = function() {
     {a:"A.n.p.a.",b:"A.N.P.A"},
     {a:";-",b:"; "},
     {a:".;",b:"."},
-    {a:"J.a.",b:"J.A."}
+    {a:"J.a.",b:"J.A."},
+    {a:"Republi.",b:"Republic."},
+    {a:". t", b:". T"},
+    {a:"Futur.",b:"Future."},
+    {a:"Wilso.",b:"Wilson."},
+    {a:"Outpu.",b:"Output."}
   ]
     .forEach(function(d,i){
       str = replaceAll(str, d.a, d.b);    
